80a1b734618e3831e67a3508f5dd2a0693e56d2d
swaranjalsingh <swaranjal3905@gmail.com>
2023-11-08 19:05:07 +0530

    1df

---
+++ CreateVM.ps1
@@ -0,0 +1,15 @@
+﻿Connect-AzAccount
+
+$rg = "Swara-RG"
+$loc = "centralindia"
+$vmName = "Swara-VM"
+$cred = Get-Credential -Message "enter username and p"
+
+#New-AzResourceGroup -Name $rg -Location $loc
+
+
+New-AzVM `
+-ResourceGroupName $rg `
+-Location $loc `
+-Name "Trial1" `
+-Credential $cred 
